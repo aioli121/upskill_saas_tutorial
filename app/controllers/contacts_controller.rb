9c3f60f6b8fd129dde8ed600f59a54aaeae33b6d
Alexander Oliver <alexanderdoliver@gmail.com>
2021-03-30 20:49:18 +0000

    Added comments to ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,16 @@
 class ContactsController < ApplicationController
+
+  # GET request for entering form info
   def new
     @contact = Contact.new
   end
 
+  # POST request for storing Contact in db
   def create
+    # Assign several vars at once with contact_params helper
     @contact = Contact.new(contact_params)
+
+    # Try saving to db & store flash message to confirm success to user
     if @contact.save
       name = params[:contact][:name]
       email = params[:contact][:email]
@@ -15,10 +21,12 @@ class ContactsController < ApplicationController
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
     end
+
     redirect_to new_contact_path
   end
 
   private
+    # To collect form data, do the following to whitelist the form fields
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
